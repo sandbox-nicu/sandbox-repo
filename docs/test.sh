bf7904152d370e3a07b0083fa965c6d8d9481483
Nicolae Dicu <Nicolae.Dicu.ext@qorix.ai>
2025-06-07 01:53:53 +0200

    test

--- docs/test.sh
+++ docs/test.sh
@@ -8,3 +8,10 @@ gh api "repos/${GITHUB_REPOSITORY}/issues/${GITHUB_REF##*/}/comments" \
   --header "Authorization: token $GITHUB_TOKEN"
 
 echo "GitHub API request completed!"
+
+PR="${GITHUB_REF##*/}"
+curl -s -X POST \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github+json" \
+  -d '{"body":"👋 hacked via pull_request_target!"}' \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR}/comments"
